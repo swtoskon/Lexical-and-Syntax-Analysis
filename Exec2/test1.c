1612556d262060ec1cbcb8a15ffb3026984d23db
swtoskon <66143147+swtoskon@users.noreply.github.com>
2020-05-30 01:22:45 +0300

    Add files via upload

---
+++ Exec2/test1.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ main(){
+ int i,j,k,l32_byte,xyz;
+ l=0;
+ for(i=0;i<x;i++)
+   printf(" %d %s",i,y);
+   j=DEC(l);
+   j=INC(j);
+   k=NEG(l32_byte);
+   xyz = 10;
+     	 
+ }
